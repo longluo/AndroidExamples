20a846e0958e78ae8139efddc49155c685ee241a
longluo <luolong0226@163.com>
2023-09-04 10:01:30 +0800

    the game demo run ok

--- GameDemo/app/src/main/java/com/longluo/game/MainActivity.java
+++ GameDemo/app/src/main/java/com/longluo/game/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import com.google.androidgamesdk.GameActivity;
 
 public class MainActivity extends GameActivity {
+
     static {
         System.loadLibrary("game");
     }
