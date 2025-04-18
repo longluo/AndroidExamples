97d57f689eda2672c7861450a5c9b865faa24745
longluo <luolong0226@163.com>
2025-04-18 17:17:37 +0800

    the android room project.

---
+++ room-with-a-view/app/src/main/java/com/example/android/roomwordssample/WordListAdapter.java
@@ -0,0 +1,55 @@
+package com.example.android.roomwordssample;
+
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+
+
+public class WordListAdapter extends ListAdapter<Word, WordViewHolder> {
+
+    public WordListAdapter(@NonNull DiffUtil.ItemCallback<Word> diffCallback) {
+        super(diffCallback);
+    }
+
+    @Override
+    public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return WordViewHolder.create(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+        Word current = getItem(position);
+        holder.bind(current.getWord());
+    }
+
+    static class WordDiff extends DiffUtil.ItemCallback<Word> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull Word oldItem, @NonNull Word newItem) {
+            return oldItem == newItem;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Word oldItem, @NonNull Word newItem) {
+            return oldItem.getWord().equals(newItem.getWord());
+        }
+    }
+}
